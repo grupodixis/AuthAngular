4e2e53cda735becdfe28e03996aea9c11e2cccbd
unknown <pablo@DESKTOP-OO2DUE2.dixis.es>
2021-03-23 16:56:16 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+//Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { loggedModule } from './logged/logged.module';
 
+//Routes
+import { AppRoutingModule } from './app.routing';
+
+//Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    loggedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
